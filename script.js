d184aa247a0cded5c667056ad1e53c1ae7bd3c86
Matteo <74088595+rainnfx@users.noreply.github.com>
2023-02-23 16:36:47 -0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+// Scroll Effects
+
+// Navigation
+
+ScrollReveal().reveal('.logo', { delay: 400 });
+ScrollReveal().reveal('.buttons', { delay: 800 });
+
+// Hero
+
+ScrollReveal().reveal('.hi', { delay: 1000 });
+
+ScrollReveal().reveal('.name', { delay: 1300 });
+ScrollReveal().reveal('.subtitle', { delay: 1600 });
+ScrollReveal().reveal('.details', { delay: 1800 });
+
+// About
+
+ScrollReveal().reveal('.heading-abt', { delay: 300 });
+ScrollReveal().reveal('.words', { delay: 600 });
+
+ScrollReveal().reveal('.programming', { delay: 900 });
+ScrollReveal().reveal('.nature', { delay: 1200 });
+ScrollReveal().reveal('.brba', { delay: 1500 });
+ScrollReveal().reveal('.tech', { delay: 1800 });
+
+// Projects
+
+ScrollReveal().reveal('.heading-prjs', { delay: 300 });
+ScrollReveal().reveal('.words-prj', { delay: 600 });
+
+ScrollReveal().reveal('.my-projects', { delay: 900 });
+
+// Contact
+
+ScrollReveal().reveal('.heading-cnact', { delay: 300 });
+ScrollReveal().reveal('.words-cnact', { delay: 600 });
+
+ScrollReveal().reveal('.contact-me', { delay: 900 });
+
+// Footer
+
+ScrollReveal().reveal('.footer-img', { delay: 400 });
+ScrollReveal().reveal('.copyright', { delay: 800 });
+ScrollReveal().reveal('.social-icons', { delay: 1200 });
